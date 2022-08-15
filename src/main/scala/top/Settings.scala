6c4eb86350bdeb47c407f52f8d14a98b49c6b989
sinsanction <1602723930@qq.com>
2022-08-15 11:43:07 +0800

    update pynq setting

--- src/main/scala/top/Settings.scala
+++ src/main/scala/top/Settings.scala
@@ -48,11 +48,11 @@ object PynqSettings {
   def apply() = Map(
     "FPGAPlatform" -> true,
     "NrExtIntr" -> 3,
-    "ResetVector" -> 0x60000000L,
-    "MemMapBase" -> 0x0000000010000000L,
+    "ResetVector" -> 0x80000000L,
+    "MemMapBase" -> 0x0000000080000000L,
     "MemMapRegionBits" -> 28,
-    "MMIOBase" -> 0x00000000e0000000L,
-    "MMIOSize" -> 0x0000000020000000L
+    "MMIOBase" -> 0x0000000040600000L,
+    "MMIOSize" -> 0x0000000001000000L
   )
 }
 
